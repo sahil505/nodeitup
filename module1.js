56515bf2793a1ca69a089645a10d41250b36d94a
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-05-15 23:19:46 +0530

    1st one

--- module1.js
+++ module1.js
@@ -1,2 +1,9 @@
-var m2 = require('./module2');
-m2();
+// var m2 = require('./module2');
+// m2();
+var person = {
+	firstName : "Sahil",
+	lastName : "Khokhar",
+	age : 20
+};
+
+console.log(person);
