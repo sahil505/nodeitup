a4150634f5295c2cceb8ca0d3ffc81ae6534dcfa
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-05-20 22:17:46 +0530

    express v1

--- module1.js
+++ module1.js
@@ -3,6 +3,11 @@ var bodyParser = require('body-parser');
 var path = require('path');
 
 var app = express();
+
+app.get('/', function(request, response){
+    response.send('Hello World!');
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000!");
 });
