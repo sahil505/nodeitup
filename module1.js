6eb9067abbf7ae1830437b04a780ff3829c76968
Sahil Khokhar <sahilkhokhar505@gmail.com>
2017-05-20 23:29:15 +0530

    Express v5

--- module1.js
+++ module1.js
@@ -13,6 +13,9 @@ var app = express();
 app.use(logger);
 */
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 //body parser middleware 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
@@ -21,31 +24,15 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-var people = [
-    {
-      name: 'Sahil',
-      age: 20
-    },
-    {
-      name: 'Nimish',
-      age: 19
-    },
-    {
-      name: 'Sumit', 
-      age: 29
-    }
-] 
-
 //routehandler
 app.get('/', function(request, response){
-    response.json(people);
+    response.render('index');
 });
 
 //set port
 app.listen(3000, function() {
   console.log("Server started on port 3000!");
 });
-
 
 
 
